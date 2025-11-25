b365302e5020a14a4e7204eaa97752284cddf231
OPENTEXT\mminda <mminda@opentext.com>
2025-11-26 00:15:18 +0530

    static examples

--- src/com/test/oops/staticKeyword/Static1.java
+++ src/com/test/oops/staticKeyword/Static1.java
@@ -13,6 +13,7 @@ public class Static1 {
         System.out.println(a);
         System.out.println("Static block");
     }
+
     public static void main(String[] args) {
         System.out.println("Main method");
     }
